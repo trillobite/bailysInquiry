6e0892161c6f3fbb7f63eb48c3d3ab344120868c
Jesse Parnell <jesse@baily.com>
2019-08-05 18:03:46 -0700

    works!

--- mailer.js
+++ mailer.js
@@ -5,14 +5,35 @@
 */
 
 const nodemailer = require("nodemailer");
+const fs = require("fs");
+const env = "./pass.env"; //path to the env file.
+let pass = undefined;
 
-const mailer = (message) => {
+//load up the env password to link gmail.
+const load = async (path) => {
+	let pass = undefined;
+
+	await fs.readFile(path, (err, data) => {
+		if(err) {
+			throw err;
+		}
+
+		pass = data;		
+	});
+
+	return pass;
+};
+
+//handles the actual mailing process...
+const mailer = async (message) => {
+
+	let pass = await load(env); //get the password...
 
 	const transporter = nodemailer.createTransport({
 		service: "Gmail",
 		auth: {
 			user: "bailys.ai.helper@gmail.com",
-			pass: "bailys1992",
+			pass: pass, //password from env.
 		}
 	});
 
